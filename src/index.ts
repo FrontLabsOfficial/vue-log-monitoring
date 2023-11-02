f880613eaadd22d29f1daf8efcb8a97198af9f58
thanhho <thanhhs2703@gmail.com>
2023-11-02 14:52:05 +0700

    feat(init): init lib

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import type { ViewModel, Vue, VueOptions } from './types';
+import { formatComponentName, generateComponentTrace } from './helper';
+type UnknownFunc = (...args: unknown[]) => void;
+
+export const attachErrorHandler = (app: Vue, options: VueOptions): void => {
+  if (options.ignoreHandler) {
+    return;
+  }
+
+  const { errorHandler, warnHandler, silent } = app.config;
+  app.config.errorHandler = (error: Error, vm: ViewModel, lifecycleHook: string): void => {
+    const componentName = formatComponentName(vm, false);
+    const trace = vm ? generateComponentTrace(vm) : '';
+    const metadata: Record<string, unknown> = {
+      componentName,
+      lifecycleHook,
+      trace,
+      raw: JSON.parse(JSON.stringify(error)),
+    };
+
+    if (options.attachProps && vm) {
+      // Vue2 - $options.propsData
+      // Vue3 - $props
+      if (vm.$options && vm.$options.propsData) {
+        metadata.propsData = vm.$options.propsData;
+      } else if (vm.$props) {
+        metadata.propsData = vm.$props;
+      }
+    }
+
+    if (typeof errorHandler === 'function') {
+      (errorHandler as UnknownFunc).call(app, error, vm, lifecycleHook);
+    }
+
+    if (options.logErrors) {
+      const hasConsole = typeof console !== 'undefined';
+      const message = `Error in ${lifecycleHook}: "${error && error.toString()}"`;
+
+      if (warnHandler) {
+        (warnHandler as UnknownFunc).call(null, message, vm, trace);
+      } else if (hasConsole && !silent) {
+        // eslint-disable-next-line no-console
+        console.error(`[Vue warn]: ${message}${trace}`);
+      }
+    }
+
+    if (options.errorCallback) {
+        options.errorCallback.call(metadata);
+    }
+  };
+};
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueLogMonitoring',
+      formats: ["es", "cjs"]
+    },
+  },
+})
